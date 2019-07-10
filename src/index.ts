fc13a4d50fbeb3248514d5098d3709c271a2f74d
Stephen <stephenplusplus@users.noreply.github.com>
2019-07-10 17:41:36 -0400

    feat!: implement flow control

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,84 @@
-import * as ended from 'is-stream-ended';
 import {Transform} from 'stream';
+
+// tslint:disable-next-line no-any
+export type ArrayItem = any;
 
 /**
  * Push an array of items into a Transform stream.
- * @param array The array you want to push to the stream.
- * @param stream The Transform stream into which array items are pushed.
+ * @param getArrayFn The array you want to push to the stream.
  */
-export async function split(array: Array<{}>, stream: Transform) {
-  return new Promise<boolean>((resolve, reject) => {
-    const arr = [].slice.call(array);
-    function loopyloop() {
-      // Ensure the stream wasn't closed by the consumer.
-      const isEnded = ended(stream);
-      // Ensure al items from the array haven't been pushed.
-      const cont = !isEnded && arr.length > 0;
-      if (cont) {
-        stream.push(arr.shift());
-        // For large arrays, use setImmediate to ensure other microtasks
-        // and I/O operations have a chance to execute.
-        setImmediate(loopyloop);
-      } else {
-        resolve(isEnded);
-      }
+export class SplitArrayStream extends Transform {
+  private _ended: boolean;
+  private _getArrayFn?: Function;
+
+  constructor(getArrayFn?: Function|ArrayItem[]) {
+    super({objectMode: true});
+
+    this._ended = false;
+
+    // When the input is an array, the user just wants it split up and emitted
+    // as data events.
+    if (Array.isArray(getArrayFn)) {
+      const array = [].slice.call(getArrayFn);
+      array.push(null);
+      this._getArrayFn = () => Promise.resolve(array);
     }
-    loopyloop();
-  });
+
+    // When the input is a function, the user wants that function called each
+    // time the destination stream is ready for more data.
+    if (typeof getArrayFn === 'function') {
+      this._getArrayFn = getArrayFn;
+    }
+
+    if (typeof getArrayFn !== 'undefined') {
+      this._read = this._readFromFn.bind(this);
+    }
+  }
+
+  // tslint:disable-next-line no-any
+  end(...args: any[]) {
+    this._ended = true;
+    return super.end(...args);
+  }
+
+  async _readFromFn() {
+    let consumerStreamReady = true;
+
+    let arrayValue = await this._getArrayFn!();
+
+    if (!Array.isArray(arrayValue)) {
+      arrayValue = [arrayValue];
+    }
+
+    const array = [].slice.call(arrayValue);
+
+    while (!this._ended && consumerStreamReady && array.length > 0) {
+      consumerStreamReady = this.push(array.shift());
+    }
+
+    if (!this._ended && consumerStreamReady && array.length > 0) {
+      setImmediate(() => this._read(0));
+    }
+  }
+
+  _transform(array: ArrayItem[], enc: string, next: Function) {
+    let consumerStreamReady = true;
+
+    array = [].slice.call(array);
+
+    while (!this._ended && consumerStreamReady && array.length > 0) {
+      consumerStreamReady = this.push(array.shift());
+    }
+
+    if (this._ended) {
+      next();
+      return;
+    }
+
+    if (consumerStreamReady && array.length === 0) {
+      next();
+    } else {
+      setImmediate(() => this._transform(array, enc, next));
+    }
+  }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,7 @@
 import * as assert from 'assert';
-import * as through from 'through2';
-import {split} from '../src';
+import {Readable} from 'stream';
+
+import {SplitArrayStream} from '../src';
 
 describe('split-array-stream', () => {
   const array = [
@@ -8,36 +9,166 @@ describe('split-array-stream', () => {
     {id: 4, user: 'Stephen'}
   ];
 
-  it('should work', async () => {
-    let numDataEvents = 0;
-    const stream = through.obj();
-    stream.on('data', () => numDataEvents++);
-    const streamEnded = await split(array, stream);
-    assert.strictEqual(streamEnded, false);
-    assert.strictEqual(numDataEvents, array.length);
-  });
+  describe('stream mode', () => {
+    let arrayStream: Readable;
 
-  it('should not push more results after end', async () => {
-    const stream = through.obj();
-    const expectedNumDataEvents = 2;
-    let numDataEvents = 0;
-    stream.on('data', d => {
-      numDataEvents++;
-      if (numDataEvents === expectedNumDataEvents) {
-        stream.end();
-      }
-      if (numDataEvents > expectedNumDataEvents) {
-        throw new Error('Should not have received this event.');
-      }
+    beforeEach(() => {
+      arrayStream = new Readable({
+        objectMode: true,
+        read() {
+          this.push(array);
+          this.push(null);
+        },
+      });
     });
-    const ended = await split(array, stream);
-    assert.strictEqual(ended, true);
-    assert.strictEqual(numDataEvents, expectedNumDataEvents);
+
+    it('should work', done => {
+      let numDataEvents = 0;
+
+      arrayStream.pipe(new SplitArrayStream())
+          .on('data', () => numDataEvents++)
+          .on('end', () => {
+            assert.strictEqual(numDataEvents, array.length);
+            done();
+          });
+    });
+
+    it('should not push more results after end', done => {
+      const expectedNumDataEvents = 2;
+      let numDataEvents = 0;
+
+      const sas = new SplitArrayStream();
+
+      arrayStream.pipe(sas)
+          .on('data',
+              () => {
+                numDataEvents++;
+                if (numDataEvents === expectedNumDataEvents) {
+                  sas.end();
+                }
+                if (numDataEvents > expectedNumDataEvents) {
+                  throw new Error('Should not have received this event.');
+                }
+              })
+          .on('end', () => {
+            assert.strictEqual(numDataEvents, expectedNumDataEvents);
+            done();
+          });
+    });
+
+    it('should not modify original array', done => {
+      const expectedArray = [].slice.call(array);
+
+      arrayStream.pipe(new SplitArrayStream())
+          .on('data', () => {})
+          .on('end', () => {
+            assert.deepStrictEqual(array, expectedArray);
+            done();
+          });
+    });
   });
 
-  it('should not modify original array', async () => {
-    const expectedArray = [].slice.call(array);
-    const ended = await split(array, through.obj());
-    assert.deepEqual(array, expectedArray);
+  describe('function mode', () => {
+    let numTimesCalled: number;
+
+    const getArrayFn = () => {
+      numTimesCalled++;
+
+      if (numTimesCalled === 1) {
+        return Promise.resolve(array);
+      } else {
+        return Promise.resolve(null);
+      }
+    };
+
+    beforeEach(() => {
+      numTimesCalled = 0;
+    });
+
+    it('should work', done => {
+      let numDataEvents = 0;
+
+      new SplitArrayStream(getArrayFn)
+          .on('data', () => numDataEvents++)
+          .on('end', () => {
+            assert.strictEqual(numDataEvents, array.length);
+            done();
+          });
+    });
+
+    it('should not push more results after end', done => {
+      const expectedNumDataEvents = 2;
+      let numDataEvents = 0;
+
+      const sas =
+          new SplitArrayStream(getArrayFn)
+              .on('data',
+                  () => {
+                    numDataEvents++;
+                    if (numDataEvents === expectedNumDataEvents) {
+                      sas.end();
+                    }
+                    if (numDataEvents > expectedNumDataEvents) {
+                      throw new Error('Should not have received this event.');
+                    }
+                  })
+              .on('end', () => {
+                assert.strictEqual(numDataEvents, expectedNumDataEvents);
+                done();
+              });
+    });
+
+    it('should not modify original array', done => {
+      const expectedArray = [].slice.call(array);
+
+      new SplitArrayStream(getArrayFn).on('data', () => {}).on('end', () => {
+        assert.deepStrictEqual(array, expectedArray);
+        done();
+      });
+    });
+  });
+
+  describe('array mode', () => {
+    it('should work', done => {
+      let numDataEvents = 0;
+
+      new SplitArrayStream(array)
+          .on('data', () => numDataEvents++)
+          .on('end', () => {
+            assert.strictEqual(numDataEvents, array.length);
+            done();
+          });
+    });
+
+    it('should not push more results after end', done => {
+      const expectedNumDataEvents = 2;
+      let numDataEvents = 0;
+
+      const sas =
+          new SplitArrayStream(array)
+              .on('data',
+                  () => {
+                    numDataEvents++;
+                    if (numDataEvents === expectedNumDataEvents) {
+                      sas.end();
+                    }
+                    if (numDataEvents > expectedNumDataEvents) {
+                      throw new Error('Should not have received this event.');
+                    }
+                  })
+              .on('end', () => {
+                assert.strictEqual(numDataEvents, expectedNumDataEvents);
+                done();
+              });
+    });
+
+    it('should not modify original array', done => {
+      const expectedArray = [].slice.call(array);
+
+      new SplitArrayStream(array).on('data', () => {}).on('end', () => {
+        assert.deepStrictEqual(array, expectedArray);
+        done();
+      });
+    });
   });
 });
